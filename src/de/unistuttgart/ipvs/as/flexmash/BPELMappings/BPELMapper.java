3682227d2440f2173ae394cd5a4589914472f1d1
KBayoumy <khaled_mahrous@live.com>
2017-02-14 22:41:38 +0100

    reference update
    updating the imports and package name for BPMN

--- src/de/unistuttgart/ipvs/as/flexmash/BPELMappings/BPELMapper.java
+++ src/de/unistuttgart/ipvs/as/flexmash/BPELMappings/BPELMapper.java
@@ -1,4 +1,4 @@
-package de.unistuttgart.ipvs.as.flexmash.BPELMappings;
+package de.unistuttgart.ipvs.as.flexmash.bpelmappings;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -9,35 +9,35 @@ import java.util.Map;
 import javax.servlet.Servlet;
 import javax.servlet.ServletContext;
 
-
 /**
  * A class to manage the BPEL syntax with .txt files within the project
  * 
- * @author mahrous
+ * @author KMahrous
  *
  */
 public class BPELMapper {
 
-	
 	/**
+	 * Method for getting the BPEL syntax from the file based processes
 	 * 
 	 * @param ServiceName
 	 * @return BPEL syntax
 	 * @throws IOException
 	 */
-	public  static String getBPELConfig(String ServiceName) throws IOException{
+	public static String getBPELConfig(String ServiceName) throws IOException {
 
-		String filepath =System.getenv("FLEXMASH")+"/files/BPELTemplates/"+ServiceName.toLowerCase()+"_template.txt";
+		String filepath = System.getenv("FLEXMASH") + "/files/BPELTemplates/"
+				+ ServiceName.toLowerCase() + "_template.txt";
 		StringBuilder sb = new StringBuilder();
-		try (BufferedReader br = new BufferedReader(new FileReader(filepath));) 
-		{	    
-		    String line = br.readLine();
-		    
-		    while (line != null) {
-		        sb.append(line);
-		        sb.append(System.lineSeparator());
-		        line = br.readLine();
-		    }
+		try (BufferedReader br = new BufferedReader(
+				new FileReader(filepath));) {
+			String line = br.readLine();
+
+			while (line != null) {
+				sb.append(line);
+				sb.append(System.lineSeparator());
+				line = br.readLine();
+			}
 		}
 		return sb.toString();
 	}
